5835c699b011598fbdf9434f7ec9fbfb24c54345
Anastasiia Luneva <anastasiya.luneva@gmail.com>
2023-05-21 22:47:05 +0300

    added test box tests

---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+   @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("[id=userName]").setValue("pupa");
+        $("[id=userEmail]").setValue("lupa@gmail.com");
+        $("[id=currentAddress]").setValue("rus - address 1");
+        $("[id=permanentAddress]").setValue("usa - address 2");
+        $("[id=submit]").click();
+
+        $("#output").shouldHave(text("pupa"),
+                text("lupa@gmail.com"),
+                text("rus - address 1"),
+                text("usa - address 2"));
+    }
+}
