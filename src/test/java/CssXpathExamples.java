8ce73995edbc75b21f6616ae2bc43bbbe3754610
Anastasiia Luneva <anastasiya.luneva@gmail.com>
2023-05-24 00:06:59 +0300

    added cssXPath

---
+++ src/test/java/CssXpathExamples.java
@@ -0,0 +1,33 @@
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class CssXpathExamples {
+    void sssXpathExamples( {
+        //<input id="userEmail">
+        $("[id=userEmail]").setValue("lupa@gmail.com");
+        $("[#userEmail]").setValue("lupa@gmail.com");
+        $("[input#userEmail]").setValue("lupa@gmail.com");
+        $x("//*[@id='email']");
+        $x("//input[@id='email']");
+
+        $(byName("email")).setValue("lupa@gmail.com");
+
+        //<input class="mr-sm-2 form-control">
+        $("[class=mr-sm-2 form-control]").setValue("lupa@gmail.com");
+        $("[.mr-sm-2 form-control]").setValue("lupa@gmail.com");
+        $("[input.mr-sm-2 form-control]").setValue("lupa@gmail.com");
+        $x("//input[@class='mr-sm-2 form-control']");
+
+        //элемент внутри элемента
+        $(".inputtext").$(".form-control").setValue("lupa@gmail.com");
+
+        //поиск по тексту
+        $x("//div[text()='Hello!']").click();
+        $(byText("Hello!"));
+
+        //по куску текста
+        $(withText("ello"))
+
+    })
+}
