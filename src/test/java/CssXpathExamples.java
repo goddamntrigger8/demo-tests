f8926e1bf338b64cdf27c0e6e541a4c4e60de642
Anastasiia Luneva <anastasiya.luneva@gmail.com>
2023-06-02 22:54:36 +0300

    add form test

--- src/test/java/CssXpathExamples.java
+++ src/test/java/CssXpathExamples.java
@@ -3,19 +3,19 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class CssXpathExamples {
-    void сssXpathExamples() {
-        //<input id="userEmail">
+    void cssXpathExamples() {
+        //поиск по <input id="userEmail">
         $("[id=userEmail]").setValue("lupa@gmail.com");
-        $("[#userEmail]").setValue("lupa@gmail.com");
+        $("[#userEmail]").setValue("lupa@gmail.com"); // можно id заменять на #
         $("[input#userEmail]").setValue("lupa@gmail.com");
         $x("//*[@id='email']");
         $x("//input[@id='email']");
 
         $(byName("email")).setValue("lupa@gmail.com");
 
-        //<input class="mr-sm-2 form-control">
+        //поиск по <input class="mr-sm-2 form-control">
         $("[class=mr-sm-2 form-control]").setValue("lupa@gmail.com");
-        $("[.mr-sm-2 form-control]").setValue("lupa@gmail.com");
+        $("[.mr-sm-2 form-control]").setValue("lupa@gmail.com"); // можно class заменять на .
         $("[input.mr-sm-2 form-control]").setValue("lupa@gmail.com");
         $x("//input[@class='mr-sm-2 form-control']");
 
@@ -28,6 +28,23 @@ public class CssXpathExamples {
 
         //по куску текста
         $(withText("ello"));
+
+        //поиск родительского элемента
+        $("").parent();
+
+        //поиск родителя родителя
+        $("").parent().parent();
+
+        //поиск ближайшего элемента
+        $("").closest("");
+
+        //поиск родственного элемента
+        $("").closest("").sibling(0);
+
+        //поиск ребенка элемента
+        $("div div"); //любой ребенок div, берется самый первый в дереве с таким названием
+        $("div>div"); //первый ребенок div с таким названием
+
 
     }
 }
--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,14 +1,11 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static java.lang.Thread.sleep;
 
 public class PracticeFormTests {
     @BeforeAll
@@ -23,21 +20,33 @@ public class PracticeFormTests {
         $("[id=firstName]").setValue("pupa"); //First Name
         $("[id=lastName]").setValue("lupa"); //Last Name
         $("[id=userEmail]").setValue("lupa@gmail.com"); // Email
-        //$(By.className("col-md-9 col-sm-12")).selectRadio("Male");//Gender TODO
+        $("[id=genterWrapper]").$(byText("Male")).click(); //Gender
         $("[id=userNumber]").setValue("9578786765"); //Mobile
-        //Date of Birth TODO
-        //$("[id=subjectsContainer]").setValue("lupa@gmail.com"); //Subjects - не работает пока TODO
-        //Hobbies TODO
-        //Picture TODO
-        $("[id=currentAddress]").setValue("rus - address 1"); //Current Address TODO
-        //State TODO
-        //City TODO
+        $("#dateOfBirthInput").click(); //Date of Birth
+        $(".react-datepicker__month-dropdown-container").$(byText("March")).click();
+        $(".react-datepicker__year-dropdown-container").$(byText("1980")).click();
+        $(".react-datepicker__month-container").$(byText("13")).click();
+        $("#subjectsInput").setValue("English").pressEnter(); //Subjects
+        $("[id=hobbiesWrapper]").$(byText("Sports")).click(); //Hobbies
+        $("[id=hobbiesWrapper]").$(byText("Music")).click(); //Hobbies
+        $("#uploadPicture").uploadFromClasspath("smudge-the-viral-cat.jpg");//Picture
+        $("[id=currentAddress]").setValue("rus - address 1"); //Current Address
+        $("#state").click(); //State
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        $("#city").click(); //City
+        $("#stateCity-wrapper").$(byText("Agra")).click();
 
         $("[id=submit]").click();
 
-        $("#output").shouldHave(text("pupa"),
+        $("[class=modal-content]").shouldHave(text("pupa"),
                 text("lupa@gmail.com"),
-                text("rus - address 1"),
-                text("usa - address 2"));
+                text("lupa"),
+                text("Male"),
+                text("9578786765"),
+                text("March"),
+                text("Sports"),
+                text("Music"),
+                text("Uttar Pradesh"),
+                text("Agra"));
     }
 }
