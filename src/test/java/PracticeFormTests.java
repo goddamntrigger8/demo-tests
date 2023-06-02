35cf97344e7d095d4b8193100f1ef4360b207de1
Anastasiia Luneva <anastasiya.luneva@gmail.com>
2023-06-03 00:22:33 +0300

    add sleep

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
     @BeforeAll
@@ -48,5 +47,6 @@ public class PracticeFormTests {
                 text("Music"),
                 text("Uttar Pradesh"),
                 text("Agra"));
+        sleep(5000);
     }
 }
