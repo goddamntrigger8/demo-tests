c16f125e499a52dda8607b2c32b83f4d7cae77e5
Anastasiia Luneva <goddamn@sakura-haruno.ru>
2023-06-06 23:27:16 +0300

    added one more github test

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -25,7 +25,19 @@ public class GitHubTest {
         // Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $(byText("Soft assertions")).click();
         $("[class=markdown-body]").shouldHave(text("Using JUnit5"));
-
         sleep(3000);
     }
+
+    @Test
+    void searchSolutionsTest() {
+        // На главной странице GitHub выберите меню Solutions -> Enterprise с помощью команды hover для Solutions
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $("[href='/enterprise']").click();
+        //Убедитесь что загрузилась нужная страница (например что заголовок - "Build like the best."
+        $("[class='h1-mktg mb-3 color-fg-default']").shouldHave(text("Build like the best"));
+        sleep(3000);
+    }
+
+
 }
